5adcd53b955c5abef52d8ad7c008fd60831ae26b
Jordan Miller <ramnadroj@gmail.com>
2022-11-16 15:58:22 -0500

    upload progress modal

--- src/component/upload_count.jsx
+++ src/component/upload_count.jsx
@@ -1,23 +1,13 @@
 // @flow 
 import * as React from 'react';
-import { useRecoilValue } from 'recoil';
-import { uploadPercent } from '../atoms';
 
-export const UploadCount = ({count = 0}) => {
-
-    const percent = useRecoilValue(uploadPercent);
+export const UploadCount = ({ count = 0 }) => {
 
     return (
-        // count > 0 ?
-        //     <span className="flex absolute -top-3 -right-3 py-1 px-2 text-white bg-red-600 rounded-full">
-        //         <p>{count}</p>
-        //     </span> :
-        //     <></>
-
         count > 0 ?
-        <span className="flex absolute -top-2 -right-2 py-1 px-2 text-white bg-red-600 rounded-full">
-            <p>{count}</p>
-        </span> :
-        <></>
+            <span className="flex absolute -top-2 -right-2 py-1 px-2 text-white bg-red-600 rounded-full">
+                <p>{count}</p>
+            </span> :
+            <></>
     );
 };
